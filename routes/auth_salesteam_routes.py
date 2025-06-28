bd136539fb05ba833d530bd12a320b06e836df97
TamirChaimaa <chaimaatamir43@gmail.com>
2025-06-28 10:34:44 +0100

    Add Sign in dans Signup for Salesteam 1

--- routes/auth_salesteam_routes.py
+++ routes/auth_salesteam_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from controllers.auth_salesteam_controllers import SalesteamAuthController
 
 # Create a new Blueprint for authentication routes
-auth_bp = Blueprint('auth', __name__)
+auth_bp = Blueprint('/salesteam/auth', __name__)
 
 # Register the signup route with POST method
 # When a POST request is made to /signup, the signup method of AuthController is called
