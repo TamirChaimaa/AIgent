5fbc4f9d30be5e5e4ad83857c6425f0b7fb7d7cc
TamirChaimaa <chaimaatamir43@gmail.com>
2025-06-29 22:23:02 +0100

    Api Raiway14

--- services/chat_client.py
+++ services/chat_client.py
@@ -13,7 +13,7 @@ class ChatClient:
         configuring the generative AI client, and setting the model.
         """
         # Get the Gemini API key from environment variables change tehe api key
-        self.api_key = "AIzaSyBttu6TAwzqOS3iBW_Ma3t54Jln2FY9GTE"
+        self.api_key = "AAIzaSyBTN9z44rhX6ZXuoHw4j0OhwbGVUKqM4XA"
         
         # If the API key is not set, raise an error
         if not self.api_key:
